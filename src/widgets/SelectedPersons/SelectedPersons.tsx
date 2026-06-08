ffbe60b344942825b2bb9662aaddeeb34c34fa10
E. Belousov <visneviy.secret@yandex.ru>
2026-06-08 18:18:24 +0300

    Refactor duplicate

--- src/widgets/SelectedPersons/SelectedPersons.tsx
+++ src/widgets/SelectedPersons/SelectedPersons.tsx
@@ -2,16 +2,13 @@ import { PersonList } from "@/entities/Person/PersonList";
 import { Window } from "@/entities/Window/Window";
 import { useAppSelector } from "@/store/hooks";
 import { selectSelectedPersons } from "@/store/persons/personsSelectors";
-import styles from "./SelectedPersons.module.scss";
 
 export function SelectedPersons() {
   const persons = useAppSelector(selectSelectedPersons);
 
   return (
-    <div className={styles.widget}>
-      <Window title="Selected">
-        <PersonList persons={persons} emptyText="No selected persons" />
-      </Window>
-    </div>
+    <Window title="Selected">
+      <PersonList persons={persons} emptyText="No selected persons" />
+    </Window>
   );
 }
--- src/widgets/UnselectedPersons/UnselectedPersons.tsx
+++ src/widgets/UnselectedPersons/UnselectedPersons.tsx
@@ -2,16 +2,13 @@ import { PersonList } from "@/entities/Person/PersonList";
 import { Window } from "@/entities/Window/Window";
 import { useAppSelector } from "@/store/hooks";
 import { selectUnselectedPersons } from "@/store/persons/personsSelectors";
-import styles from "./UnselectedPersons.module.scss";
 
 export function UnselectedPersons() {
   const persons = useAppSelector(selectUnselectedPersons);
 
   return (
-    <div className={styles.widget}>
-      <Window title="Unselected">
-        <PersonList persons={persons} emptyText="No unselected persons" />
-      </Window>
-    </div>
+    <Window title="Unselected">
+      <PersonList persons={persons} emptyText="No unselected persons" />
+    </Window>
   );
 }
